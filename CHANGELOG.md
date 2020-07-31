4db6a491dc83d3c0bc9583066bd0d087d7be101a
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 拆分出 neditor 功能 ([](https://github.com/miaoxing/mxjs-neditor/commit/))
---
+++ index.js
@@ -0,0 +1,3 @@
+import 'plugins/ueditor/public/js/neditor.config';
+import 'plugins/ueditor/public/libs/neditor/neditor.all.min';
+import 'plugins/ueditor/public/libs/neditor/i18n/zh-cn/zh-cn';
